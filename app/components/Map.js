52e48f3a6bcb96b87e883623a741e5ad95d893f7
Berke SASA <berkesasa@Berke-MacBook-Air.local>
2023-09-08 14:05:54 +0300

    map marker uploaded

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,9 +1,13 @@
 import MapView, { Marker, Popup } from 'react-map-gl/maplibre';
+
 import { getCenter } from 'geolib';
 import maplibregl from 'maplibre-gl';
+import { useState } from 'react';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 function Map({ results }) {
+
+    const [selectedLocation, setSelectedLocation] = useState({});
 
     const coordinates = results.map((result) => ({
         longitude: result.long,
@@ -11,6 +15,7 @@ function Map({ results }) {
     }))
 
     const center = getCenter(coordinates)
+    const markerOffset = [-20, -10]
 
     return (
         <MapView
@@ -19,19 +24,46 @@ function Map({ results }) {
                 latitude: center.latitude,
                 zoom: 12
             }}
-            style={{width: "100%", height: "100%"}}
+            style={{ width: "100%", height: "100%" }}
             mapStyle={`https://api.maptiler.com/maps/067adaad-17b2-4574-9e2c-cdd91074ecbb/style.json?key=${process.env.maplibre_key}`}
         >
-            {results.map(result => (
-                
+            {results.map((result) => (
+
                 <div key={result.long}>
-                    
+
                     <Marker
+                        onClick={() => setSelectedLocation(result)}
+                        className='cursor-pointer'
                         longitude={result.long}
                         latitude={result.lat}
                         color='#00A699'
-                    >
-                    </Marker>
+                        offset={markerOffset}
+                        aria-label="push-pin"
+                    />
+                    {selectedLocation.long === result.long || (
+                        <Popup
+                            onClose={() => setSelectedLocation({})}
+                            closeOnClick={true}
+                            offset={markerOffset}
+                            latitude={result.lat}
+                            longitude={result.long}
+                        >
+                            {result.title}
+                        </Popup>)
+
+                    }
+
+                    {/* {selectedLocation ? (
+                        <Popup
+                            offset={markerOffset}
+                            // onClose={() => setSelectedLocation({})}
+                            // closeOnClick={true}
+                            latitude={result.lat}
+                            longitude={result.long}
+                        >
+                            {result.title}
+                        </Popup>
+                    ):(false)} */}
                 </div>
             ))}
 

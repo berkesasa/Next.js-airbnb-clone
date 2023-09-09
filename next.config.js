ccebb7f7fa8aaf329c3a983c7d24f9ada24a47f9
Berke SASA <berkesasa@Berke-MacBook-Air.local>
2023-09-09 14:09:41 +0300

    ignoring nextconfigjs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     images: {
         domains: ["upload.wikimedia.org", "https://a0.muscache.com", "links.papareact.com"]
+    },
+    env: {
+        maplibre_key: "simaFby6FyxpJTn5Uc0v"
     }
 }
 

0ce4c2fc5a17a9babc5c7e3bf7020a18896481a0
Berke SASA <64038032+berkesasa@users.noreply.github.com>
2023-09-09 14:07:34 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ["upload.wikimedia.org", "https://a0.muscache.com", "links.papareact.com"]
-    }
-}
-
-module.exports = nextConfig
